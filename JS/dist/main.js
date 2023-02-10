0906d0fd71e2bdf8625ab515a6d4e58c13a78bf2
mohamed haithem <mohamedhyssm2019@gmail.com>
2023-02-10 19:42:46 +0200

    article structure finished

--- JS/dist/main.js
+++ JS/dist/main.js
@@ -1,3 +1,15 @@
 "use strict";
-let links = Array.from(document.querySelectorAll("nav ul li a"));
+let firstArticelContainer = document.querySelector(".article-1 .container"), secondArticelContainer = document.querySelector(".article-2 .container"), ideaHeading = document.getElementById("main-heading");
+window.addEventListener("scroll", () => {
+    if (window.scrollY <= 300) {
+        ideaHeading.classList.add("leftToRight");
+    }
+    else if (window.scrollY <= 500) {
+        firstArticelContainer.classList.add("fadeInDown");
+    }
+    else if (window.scrollY <= 800) {
+        secondArticelContainer.classList.add("fadeInDown");
+    }
+    console.log(window.scrollY);
+});
 //# sourceMappingURL=main.js.map
--- JS/src/main.ts
+++ JS/src/main.ts
@@ -1,3 +1,16 @@
-let links = <HTMLAnchorElement[]> Array.from(document.querySelectorAll("nav ul li a"));
+let firstArticelContainer = <HTMLDivElement>document.querySelector(".article-1 .container"),
+  secondArticelContainer = <HTMLDivElement>document.querySelector(".article-2 .container"),
+  ideaHeading = <HTMLHeadElement>document.getElementById("main-heading")
 
-
+window.addEventListener("scroll", () => {
+  if (window.scrollY <= 300) {
+    ideaHeading.classList.add("leftToRight")
+  }
+  else if (window.scrollY <= 500) {
+    firstArticelContainer.classList.add("fadeInDown")
+  }
+  else if (window.scrollY <= 800) {
+    secondArticelContainer.classList.add("fadeInDown")
+  }
+  console.log(window.scrollY)
+})
