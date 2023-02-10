3c7b3fd40f5036744417f1a32b0826876ceca021
mohamed haithem <mohamedhyssm2019@gmail.com>
2023-02-10 21:35:51 +0200

    article structure finished

--- JS/dist/main.js
+++ JS/dist/main.js
@@ -1,26 +1,35 @@
 "use strict";
-let firstArticelContainer = document.querySelector(".article-1 .container"), secondArticelContainer = document.querySelector(".article-2 .container"), ideaHeading = document.getElementById("main-heading");
+let firstIdeaContainer = document.querySelector(".article-1 .container"), secondIdeaContainer = document.querySelector(".article-2 .container"), firstFeatureContainer = document.querySelector(".feature-1 .container"), secondFeatureContainer = document.querySelector(".feature-2 .container"), ideaHeading = document.getElementById("main-heading-idea"), featureHeading = document.getElementById("main-heading-feature");
 let settingScroll = 0;
-if (window.innerWidth <= 767) {
-    settingScroll = 400;
+if (window.innerWidth <= 600) {
+    settingScroll = 300;
 }
 window.addEventListener("resize", () => {
-    if (window.innerWidth <= 767) {
-        settingScroll = 400;
+    if (window.innerWidth <= 600) {
+        settingScroll = 300;
     }
     else {
         settingScroll = 0;
     }
 });
 window.addEventListener("scroll", () => {
-    if (window.scrollY <= 300) {
+    if (window.scrollY >= 300) {
         ideaHeading.classList.add("leftToRight");
     }
-    else if (window.scrollY <= (500 + settingScroll)) {
-        firstArticelContainer.classList.add("fadeInDown");
+    if (window.scrollY >= 500) {
+        firstIdeaContainer.classList.add("fadeInDown");
     }
-    else if (window.scrollY <= (800 + settingScroll)) {
-        secondArticelContainer.classList.add("fadeInDown");
+    if (window.scrollY >= (800 + settingScroll)) {
+        secondIdeaContainer.classList.add("fadeInDown");
+    }
+    if (window.scrollY >= (1200 + settingScroll)) {
+        featureHeading.classList.add("leftToRight");
+    }
+    if (window.scrollY >= (1500 + settingScroll)) {
+        firstFeatureContainer.classList.add("fadeInDown");
+    }
+    if (window.scrollY >= (1800 + settingScroll)) {
+        secondFeatureContainer.classList.add("fadeInDown");
     }
 });
 //# sourceMappingURL=main.js.map
--- JS/src/main.ts
+++ JS/src/main.ts
@@ -1,16 +1,22 @@
-let firstArticelContainer = <HTMLDivElement>document.querySelector(".article-1 .container"),
-  secondArticelContainer = <HTMLDivElement>document.querySelector(".article-2 .container"),
-  ideaHeading = <HTMLHeadElement>document.getElementById("main-heading")
+// Ideas Article
+let firstIdeaContainer = <HTMLDivElement>document.querySelector(".article-1 .container"),
+  secondIdeaContainer = <HTMLDivElement>document.querySelector(".article-2 .container"),
+  // Feature Articles
+  firstFeatureContainer = <HTMLDivElement>document.querySelector(".feature-1 .container"),
+  secondFeatureContainer = <HTMLDivElement>document.querySelector(".feature-2 .container"),
+  // main Heading
+  ideaHeading = <HTMLHeadElement>document.getElementById("main-heading-idea"),
+  featureHeading = <HTMLHeadElement>document.getElementById("main-heading-feature")
 
 let settingScroll: number= 0;
 
-if (window.innerWidth <= 767) {
-  settingScroll= 400
+if (window.innerWidth <= 600) {
+  settingScroll= 300
 }
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth <= 767) {
-    settingScroll= 400
+  if (window.innerWidth <= 600) {
+    settingScroll= 300
   }
   else {
     settingScroll = 0
@@ -19,13 +25,23 @@ window.addEventListener("resize", () => {
 
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY <= 300) {
+  if (window.scrollY >= 300) {
     ideaHeading.classList.add("leftToRight")
   }
-  else if (window.scrollY <= (500 + settingScroll)) {
-    firstArticelContainer.classList.add("fadeInDown")
+  if (window.scrollY >= 500) {
+    firstIdeaContainer.classList.add("fadeInDown")
   }
-  else if (window.scrollY <= (800 + settingScroll)) {
-    secondArticelContainer.classList.add("fadeInDown")
+  if (window.scrollY >= (800 + settingScroll)) {
+    secondIdeaContainer.classList.add("fadeInDown")
+  }
+  if (window.scrollY >= (1200 + settingScroll)) {
+    featureHeading.classList.add("leftToRight")
+  }
+  if (window.scrollY >= (1500 + settingScroll)) {
+    firstFeatureContainer.classList.add("fadeInDown")
+  }
+
+  if (window.scrollY >= (1800 + settingScroll)) {
+    secondFeatureContainer.classList.add("fadeInDown")
   }
 })
