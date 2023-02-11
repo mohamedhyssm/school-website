888897fdc15b1d1791f432d867a48e46b345ba33
mohamed haithem <mohamedhyssm2019@gmail.com>
2023-02-11 19:37:30 +0200

    footer and scroll ts

--- JS/dist/main.js
+++ JS/dist/main.js
@@ -1,44 +1,74 @@
 "use strict";
-let burgerIcon = document.getElementById("burgerIcon"), ulNavBar = document.getElementById("ulNavBar"), liNavBar = Array.from(ulNavBar.children);
-let firstIdeaContainer = document.querySelector(".article-1 .container"), secondIdeaContainer = document.querySelector(".article-2 .container"), firstFeatureContainer = document.querySelector(".feature-1 .container"), secondFeatureContainer = document.querySelector(".feature-2 .container"), ideaHeading = document.getElementById("main-heading-idea"), featureHeading = document.getElementById("main-heading-feature"), settingScroll = 0;
-if (window.innerWidth <= 600) {
-    settingScroll = 300;
-    liNavBar.forEach((li) => li.addEventListener("click", function () {
-        ulNavBar.classList.remove("ul-flex");
-    }));
-}
+let burgerIcon = document.getElementById("burgerIcon"), ulNavBar = document.getElementById("ulNavBar"), anchorNavBar = Array.from(document.querySelectorAll("#ulNavBar li a")), ideaLink = document.getElementById("ideaLink"), featurelink = document.getElementById("featurelink"), firstIdea = document.getElementById("article-1"), firstFeature = document.getElementById("feature-1"), firstIdeaContainer = document.querySelector(".article-1 .container"), secondIdeaContainer = document.querySelector(".article-2 .container"), firstFeatureContainer = document.querySelector(".feature-1 .container"), secondFeatureContainer = document.querySelector(".feature-2 .container"), ideaHeading = document.getElementById("main-heading-idea"), featureHeading = document.getElementById("main-heading-feature");
+handleNavBar();
 burgerIcon.addEventListener("click", () => {
     ulNavBar.classList.toggle("ul-flex");
 });
+ideaLink.onclick = function (e) {
+    e.preventDefault();
+    handleScroll(firstIdea);
+};
+featurelink.onclick = function (e) {
+    e.preventDefault();
+    handleScroll(firstFeature);
+};
 window.addEventListener("resize", () => {
-    if (window.innerWidth <= 600) {
-        settingScroll = 300;
-        liNavBar.forEach((li) => li.addEventListener("click", function () {
-            ulNavBar.classList.remove("ul-flex");
-        }));
-    }
-    else {
-        settingScroll = 0;
-    }
+    handleNavBar();
 });
+window.onload = () => {
+    scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
 window.addEventListener("scroll", () => {
     if (window.scrollY >= 300) {
         ideaHeading.classList.add("leftToRight");
     }
-    if (window.scrollY >= 500) {
+    if (window.scrollY >= (firstIdeaContainer.offsetTop - 400)) {
         firstIdeaContainer.classList.add("fadeInDown");
     }
-    if (window.scrollY >= (800 + settingScroll)) {
+    if (window.scrollY >= (secondIdeaContainer.offsetTop - 400)) {
         secondIdeaContainer.classList.add("fadeInDown");
     }
-    if (window.scrollY >= (1200 + settingScroll)) {
+    if (window.scrollY >= (secondIdeaContainer.offsetTop - 400)) {
         featureHeading.classList.add("leftToRight");
     }
-    if (window.scrollY >= (1500 + settingScroll)) {
+    if (window.scrollY >= (firstFeatureContainer.offsetTop - 400)) {
         firstFeatureContainer.classList.add("fadeInDown");
     }
-    if (window.scrollY >= (1800 + settingScroll)) {
+    if (window.scrollY >= (secondFeatureContainer.offsetTop - 400)) {
         secondFeatureContainer.classList.add("fadeInDown");
     }
+    if (window.scrollY <= firstIdea.offsetTop - 90) {
+        removeActiveAnchors();
+        anchorNavBar[0].classList.add("active");
+    }
+    if (window.scrollY >= (firstIdea.offsetTop - 90)) {
+        removeActiveAnchors();
+        anchorNavBar[1].classList.add("active");
+    }
+    if (window.scrollY >= (firstFeature.offsetTop - 90)) {
+        removeActiveAnchors();
+        anchorNavBar[2].classList.add("active");
+    }
 });
+function handleNavBar() {
+    if (window.innerWidth <= 600) {
+        anchorNavBar.forEach((a) => a.addEventListener("click", function () {
+            ulNavBar.classList.remove("ul-flex");
+        }));
+    }
+}
+function handleScroll(section) {
+    scrollTo({
+        top: section.offsetTop - 90,
+        behavior: "smooth"
+    });
+}
+function removeActiveAnchors() {
+    anchorNavBar.forEach((a) => {
+        a.classList.remove("active");
+    });
+}
 //# sourceMappingURL=main.js.map
--- JS/src/main.ts
+++ JS/src/main.ts
@@ -1,59 +1,99 @@
 // nav
 let burgerIcon = <HTMLDivElement> document.getElementById("burgerIcon"),
-ulNavBar = <HTMLDivElement> document.getElementById("ulNavBar"),
-liNavBar: Element[] = Array.from(ulNavBar.children);
-// Ideas Article
-let firstIdeaContainer = <HTMLDivElement>document.querySelector(".article-1 .container"),
+  ulNavBar = <HTMLDivElement> document.getElementById("ulNavBar"),
+  anchorNavBar: Element[] = Array.from(document.querySelectorAll("#ulNavBar li a")),
+  //links
+  ideaLink = <HTMLDivElement>document.getElementById("ideaLink"),
+  featurelink = <HTMLDivElement>document.getElementById("featurelink"),
+  // Ideas Article
+  firstIdea = <HTMLDivElement>document.getElementById("article-1"),
+  firstFeature = <HTMLDivElement>document.getElementById("feature-1"),
+  firstIdeaContainer = <HTMLDivElement>document.querySelector(".article-1 .container"),
   secondIdeaContainer = <HTMLDivElement>document.querySelector(".article-2 .container"),
   // Feature Articles
   firstFeatureContainer = <HTMLDivElement>document.querySelector(".feature-1 .container"),
   secondFeatureContainer = <HTMLDivElement>document.querySelector(".feature-2 .container"),
   // main Heading
   ideaHeading = <HTMLHeadElement>document.getElementById("main-heading-idea"),
-  featureHeading = <HTMLHeadElement>document.getElementById("main-heading-feature"),
-  settingScroll: number= 0;
+  featureHeading = <HTMLHeadElement>document.getElementById("main-heading-feature")
 
-if (window.innerWidth <= 600) {
-  settingScroll= 300
-  liNavBar.forEach( (li) => (li as HTMLDivElement).addEventListener("click", function () {
-    ulNavBar.classList.remove("ul-flex")
-  }))
-}
+handleNavBar()
 
 burgerIcon.addEventListener("click", () => {
   ulNavBar.classList.toggle("ul-flex")
 }) 
 
+ideaLink.onclick = function (e) {
+  e.preventDefault();
+  handleScroll(firstIdea);
+}
+
+featurelink.onclick = function (e) {
+  e.preventDefault();
+  handleScroll(firstFeature)
+}
+
 window.addEventListener("resize", () => {
-  if (window.innerWidth <= 600) {
-    settingScroll= 300
-      // navBar
-    liNavBar.forEach( (li) => (li as HTMLDivElement).addEventListener("click", function () {
-    ulNavBar.classList.remove("ul-flex")
-  }))
-  }
-  else {
-    settingScroll = 0
-  }
+  handleNavBar()
 })
+
+window.onload = () => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
 
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 300) {
     ideaHeading.classList.add("leftToRight")
   }
-  if (window.scrollY >= 500) {
+  if (window.scrollY >= (firstIdeaContainer.offsetTop - 400)) {
     firstIdeaContainer.classList.add("fadeInDown")
   }
-  if (window.scrollY >= (800 + settingScroll)) {
+  if (window.scrollY >= (secondIdeaContainer.offsetTop - 400)) {
     secondIdeaContainer.classList.add("fadeInDown")
   }
-  if (window.scrollY >= (1200 + settingScroll)) {
+  if (window.scrollY >= (secondIdeaContainer.offsetTop - 400)) {
     featureHeading.classList.add("leftToRight")
   }
-  if (window.scrollY >= (1500 + settingScroll)) {
+  if (window.scrollY >= (firstFeatureContainer.offsetTop - 400)) {
     firstFeatureContainer.classList.add("fadeInDown")
   }
-  if (window.scrollY >= (1800 + settingScroll)) {
+  if (window.scrollY >= (secondFeatureContainer.offsetTop - 400)) {
     secondFeatureContainer.classList.add("fadeInDown")
   }
+  if (window.scrollY <= firstIdea.offsetTop - 90) {
+    removeActiveAnchors()
+    anchorNavBar[0].classList.add("active")
+  }
+  if (window.scrollY >= (firstIdea.offsetTop - 90)) {
+    removeActiveAnchors()
+    anchorNavBar[1].classList.add("active")
+  }
+  if (window.scrollY >= (firstFeature.offsetTop - 90)) {
+    removeActiveAnchors()
+    anchorNavBar[2].classList.add("active")
+  }
 })
+
+function handleNavBar(): void {
+  if (window.innerWidth <= 600) {
+    anchorNavBar.forEach( (a) => (a as HTMLDivElement).addEventListener("click", function () {
+    ulNavBar.classList.remove("ul-flex")
+    }))
+  }
+}
+
+function handleScroll(section: HTMLDivElement) {
+  scrollTo({
+    top: section.offsetTop - 90,
+    behavior: "smooth"
+  })
+}
+
+function removeActiveAnchors() {
+  anchorNavBar.forEach( (a) => {
+    (a as HTMLDivElement).classList.remove("active")
+  })
+}
